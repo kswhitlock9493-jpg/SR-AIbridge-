c2d69dec4b01c03c7aea26ee58b67fb59a20193d
kswhitlock9493-jpg <k.s.whitlock9493@gmail.com>
2025-09-21 22:47:27 -0500

    Add MissionLog component

---
+++ src/components/MissionLog.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MissionLog = () => (
+  <div className="placeholder">
+    <h2>🚀 Mission Log</h2>
+    <p>Live and historical mission details.</p>
+  </div>
+);
+
+export default MissionLog;
