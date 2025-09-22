ee5fbdbae352f0c68bc5d6caf4205fda7a5cf96e
kswhitlock9493-jpg <k.s.whitlock9493@gmail.com>
2025-09-21 21:01:53 -0500

    Create src/api.js with getStatus function

---
+++ src/api.js
@@ -0,0 +1,7 @@
+export async function getStatus() {
+  const response = await fetch('http://localhost:8000/status');
+  if (!response.ok) {
+    throw new Error(`HTTP ${response.status}`);
+  }
+  return await response.json();
+}
